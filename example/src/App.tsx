512bf93f11663539cc28b012b3f162cbb0757eeb
AskAlice <alice@askalice.me>
2020-11-03 12:11:35 -0700

    ree

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,40 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-flex-animations'
+import { AnimatedFlexbox } from 'react-flex-animations'
 import 'react-flex-animations/dist/index.css'
+import styled from 'styled-components';
+const Nav = styled(AnimatedFlexbox)`
+height:72px;
 
+`;
+const Test = styled.section`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    overflow: hidden;
+    margin:0;
+    padding:0;
+`;
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [hide,setHide] = React.useState(0);
+  React.useEffect(() => {
+    const t = setTimeout(()=>setHide(hide+1),2000);
+    return () => {
+      clearTimeout(t);
+    }
+  }, [hide])
+  return (<Nav component="nav" spacerComponent="div">
+      <a href="#" role="button" data-hidden={hide%4 === 1} data-ratio={0.5}>Logo</a>
+      <section className="taskSwitcher" data-hidden={hide%4 === 2}>TaskSwitcher</section>
+      <Test className="rightNav" data-hidden={hide%4 === 0} data-ratio={1}>
+        <section><span role="img" aria-label="Help">❓</span></section>
+        <section><span role="img" aria-label="Bell">🔔</span></section>
+        <section><span role="img" aria-label="Settings">⚙</span></section>
+        <section><span role="img" aria-label="User">👤</span></section>
+      </Test>
+    </Nav>
+    );
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,144 @@
-import * as React from 'react'
-import styles from './styles.module.css'
+import React, { Fragment, Children, isValidElement } from 'react'
+import styled, { keyframes } from 'styled-components'
 
 interface Props {
-  text: string
+  children?: React.ReactNode
+  component?: React.ElementType | React.Component
+  spacerComponent?: React.Component | React.ElementType
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+const shrink = keyframes`
+to {
+  flex: .01;
+  flex: .00001;
+}
+`
+
+const Spacer = styled(({ component, className }) => {
+  const Component = component?.type || component
+  console.log(Component)
+  return <Component className={className} />
+})`
+  height: 100%;
+  flex: ${(props) => props.leftOver};
+  &.hidden {
+    flex: ${(props) => props.leftOver};
+    animation: ${shrink} 500ms ease forwards;
+  }
+  &.show {
+    flex: 0.01;
+    flex: 0.00001;
+    animation: ${(props) => keyframes`to{ flex: ${props.leftOver};}`} 500ms ease
+      forwards;
+  }
+`
+
+const Spaced = styled(
+  ({
+    component,
+    children,
+    ratio,
+    spacerComponent,
+    hidden,
+    className,
+    ...rest
+  }) => {
+    console.log(className)
+    const Component = component?.type || component
+    console.log(component?.props['data-hidden'])
+    const spacing = (1 - ratio) / 2
+    return (
+      <Fragment>
+        {spacing !== 0 && (
+          <Spacer
+            className={hidden ? 'hidden' : 'show'}
+            component={spacerComponent}
+            leftOver={spacing}
+          />
+        )}
+        <Component
+          className={`${className}${hidden ? ' hidden' : ' show'}`}
+          {...rest}
+        >
+          {children}
+        </Component>
+        {spacing !== 0 && (
+          <Spacer
+            className={hidden ? 'hidden' : 'show'}
+            component={spacerComponent}
+            leftOver={spacing}
+          />
+        )}
+      </Fragment>
+    )
+  }
+)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+  flex: ${(props) => props.ratio};
+  height: 100%;
+  &.hidden {
+    flex: ${(props) => props.ratio};
+    animation: ${shrink} 500ms ease forwards;
+  }
+  &.show {
+    flex: 0.00001;
+    animation: ${(props) => keyframes`to{ flex: ${props.ratio};}`} 500ms ease
+      forwards;
+  }
+`
+
+const Flex = styled(({ component, children, className }) => {
+  const Component = component
+  return <Component className={className}>{children}</Component>
+})`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  text-align: center;
+  & *:nth-child(odd) {
+    background: #039ad1;
+  }
+  & *:nth-child(even) {
+    background: #03d19a;
+  }
+`
+
+export const AnimatedFlexbox = ({
+  children,
+  component = 'section',
+  spacerComponent = 'section',
+  ...rest
+}: Props) => {
+  console.log(children)
+  console.log(React.Children)
+
+  return (
+    <Flex
+      children={Children.map(
+        children,
+        (child) =>
+          isValidElement(child) && (
+            <Spaced
+              component={child}
+              ratio={child?.props['data-ratio'] || 1}
+              hidden={child?.props['data-hidden'] || false}
+              {...child.props}
+              spacerComponent={spacerComponent}
+            />
+          )
+      )}
+      component={component}
+      {...rest}
+    />
+  )
 }
