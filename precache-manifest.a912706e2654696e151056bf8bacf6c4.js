1179cd6d3bf34ad26a6e8e8834bf44a2d385b3f1
AskAlice <alice@askalice.me>
2021-01-24 00:04:15 -0700

    Updates

---
+++ precache-manifest.a912706e2654696e151056bf8bacf6c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ee627c4d93ac97e7bac0dc48527e480",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d1b0c8b846a57d2a5c47",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "71502e7d1ef9198a668e",
+    "url": "./static/js/2.5a56349c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.5a56349c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1b0c8b846a57d2a5c47",
+    "url": "./static/js/main.b2a6c75b.chunk.js"
+  },
+  {
+    "revision": "57b019569997874aedf7",
+    "url": "./static/js/runtime-main.a75dc611.js"
+  }
+]);
